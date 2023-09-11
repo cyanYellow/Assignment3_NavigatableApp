4e410f41f14bff2621154bd70b37ef1204e7da44
cyanYellow <williecyanyellow@gmail.com>
2023-09-11 13:41:52 -0600

    Initial Commit

---
+++ Project3_NavigatableApp/Project3_NavigatableAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project3_NavigatableAppApp.swift
+//  Project3_NavigatableApp
+//
+//  Created by Willie Green on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Project3_NavigatableAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
